de9dd30c91fd266e6a9f76089f6ddfca7a07ca85
rutvikts <rutvikts@gmail.com>
2024-05-03 04:16:42 -0500

    adding matlab files & simulation graphs

---
+++ Simulations/Matlab/LDO_VinvsTime.m
@@ -0,0 +1,38 @@
+%% Formatting
+% colordef white;
+
+% Plot with default font size 16+
+% Linewidth of 2 or 3
+set(0,'defaultAxesFontName', 'Arial');
+set(0,'defaultTextFontName', 'Arial');
+set(0,'defaultAxesFontWeight', 'Bold');
+set(0,'defaultTextFontWeight', 'Bold');
+set(0, 'DefaultTextFontSize', 8) ;
+set(0, 'DefaultAxesFontSize', 10) ;
+set(0, 'DefaultLineLineWidth', 3.5) ;
+set(0, 'DefaultStairLineWidth', 3.5) ;
+set(0, 'DefaultAxesLineWidth', 2.5) ;
+set(0, 'DefaultPatchLineWidth', 2) ;
+set(0, 'DefaultAxesXGrid', 'on') ;
+set(0, 'DefaultAxesYGrid', 'on') ;
+set(0, 'DefaultAxesBox', 'on') ;
+set(0, 'DefaultFigurePosition', [200 175 1000 600]);
+
+%% Plotting
+% modified the directory of the .txt file as needed
+filename = '/MATLAB Drive/LDO_VIN.csv';
+data = readmatrix(filename);
+
+time = data(:,1);
+vin = data(:,2);
+
+% plotting the required plots in 2 by 2 grid using subplot 
+plot(time, vin)
+
+xlabel('Time (s)')
+ylabel('Vin (V)')
+ylim([1.6, 2.0])
+title('Vin step over time')
+hold on
+
+hold off
